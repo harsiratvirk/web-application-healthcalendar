1586e8d28e721481f33f08338c56c372d4c53143
Rankatanka <hegro8348@oslomet.no>
2025-11-09 23:27:01 +0100

    Removed wrong comment from AuthController
    Removed Schedule-table. Events are now connected to Availability with a one-to-many connection

---
+++ api/Migrations/20251109222330_HealthCalendarDb_7.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class HealthCalendarDb_7 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Schedules");
+
+            migrationBuilder.AddColumn<int>(
+                name: "EventId",
+                table: "Availability",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Availability_EventId",
+                table: "Availability",
+                column: "EventId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Availability_Events_EventId",
+                table: "Availability",
+                column: "EventId",
+                principalTable: "Events",
+                principalColumn: "EventId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Availability_Events_EventId",
+                table: "Availability");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Availability_EventId",
+                table: "Availability");
+
+            migrationBuilder.DropColumn(
+                name: "EventId",
+                table: "Availability");
+
+            migrationBuilder.CreateTable(
+                name: "Schedules",
+                columns: table => new
+                {
+                    ScheduleId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    AvailabilityId = table.Column<int>(type: "INTEGER", nullable: false),
+                    EventId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Schedules", x => x.ScheduleId);
+                    table.ForeignKey(
+                        name: "FK_Schedules_Availability_AvailabilityId",
+                        column: x => x.AvailabilityId,
+                        principalTable: "Availability",
+                        principalColumn: "AvailabilityId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Schedules_Events_EventId",
+                        column: x => x.EventId,
+                        principalTable: "Events",
+                        principalColumn: "EventId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Schedules_AvailabilityId",
+                table: "Schedules",
+                column: "AvailabilityId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Schedules_EventId",
+                table: "Schedules",
+                column: "EventId");
+        }
+    }
+}
